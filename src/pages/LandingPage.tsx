dd718a01f58c0d060a6e7e33f239a55cfc67d4f0
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-09-09 17:28:40 +0200

    Added main html and css for the random cocktail container

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,29 @@ import { ReactElement } from "react";
 export function LandingPage() : ReactElement {
     return (
     <>
-        yo
+        <section id="randomCocktailContainer">
+             {/* Tar som exempel 
+                https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
+             */}
+            <figure id="randomCocktailContainer-leftSide">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Klassiche_Margarita.jpg" />
+            </figure>
+
+            <section id="randomCocktailContainer-rightSide">
+                
+                <section id="randomCocktailContainer-rightSide-top">
+                    Cocktail name
+                </section>
+
+                <section id="randomCocktailContainer-rightSide-bottom">
+                    More
+                </section>
+
+            </section>
+        </section>
+        
+
+        <button >Surprise me</button>
     </>
     )
 }
