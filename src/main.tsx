7cd2d2936661c60dfbc56112687bc10c6b8e4b84
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-09-09 16:14:33 +0200

    First commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import { RouterProvider } from 'react-router-dom'
-import { routing } from './routing.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './css/index.css';
+import { RouterProvider } from 'react-router-dom';
+import { routing } from './routing.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
