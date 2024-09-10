23ef7ed8f0704d3773062743e5a7d1efbbd9c6e1
Johan Angelstam <johan@angelstam.se>
2024-09-10 10:47:27 +0200

    Add the TheCocktailDB with endpoint search using test data.

--- src/data/TheCocktailDB.ts
+++ src/data/TheCocktailDB.ts
@@ -15,7 +15,6 @@ export interface Drink {
     imageAttribution: String | null;
     creativeCommonsConfirmed: String;
     dateModified: String;
-
 }
 
 export interface Ingredient {
@@ -27,6 +26,12 @@ export interface Ingredient {
 export async function random() {
     return new Promise<Drink>((resolve, _) => {
         resolve(DRINK_1);
+    });
+}
+
+export async function search(query: String) {
+    return new Promise<Array<Drink>>((resolve, _) => {
+        resolve([DRINK_2, DRINK_3]);
     });
 }
 
@@ -65,4 +70,76 @@ const DRINK_1: Drink = {
     imageAttribution: null,
     creativeCommonsConfirmed: "No",
     dateModified: "2017-09-07 21:50:03"
+};
+
+const DRINK_2: Drink = {
+   id: "11007",
+   drink: "Margarita",
+   drinkAlternate: null,
+   tags: ["IBA", "ContemporaryClassic"],
+   video: null,
+   category: "Ordinary Drink",
+   IBA: "Contemporary Classics",
+   alcoholic: "Alcoholic",
+   glass: "Cocktail glass",
+   instructions: "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.",
+   drinkThumb: "https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg",
+   ingredients: [
+        {
+           ingredient: "Tequila",
+           measure: "1 1\/2 oz "
+        },
+        {
+           ingredient: "Triple sec",
+           measure: "1\/2 oz "
+        },
+        {
+           ingredient: "Lime juice",
+           measure: "1 oz "
+        },
+        {
+           ingredient: "Salt",
+           measure: null
+        }
+    ],
+   imageSource: "https:\/\/commons.wikimedia.org\/wiki\/File:Klassiche_Margarita.jpg",
+   imageAttribution: "Cocktailmarler",
+   creativeCommonsConfirmed: "Yes",
+   dateModified: "2015-08-18 14:42:59"
+};
+
+const DRINK_3: Drink = {
+   id: "11118",
+   drink: "Blue Margarita",
+   drinkAlternate: null,
+   tags: null,
+   video: null,
+   category: "Ordinary Drink",
+   IBA: null,
+   alcoholic: "Alcoholic",
+   glass: "Cocktail glass",
+   instructions: "Rub rim of cocktail glass with lime juice. Dip rim in coarse salt. Shake tequila, blue curacao, and lime juice with ice, strain into the salt-rimmed glass, and serve.",
+   drinkThumb: "https:\/\/www.thecocktaildb.com\/images\/media\/drink\/bry4qh1582751040.jpg",
+   ingredients: [
+        {
+           ingredient: "Tequila",
+           measure: "1 1\/2 oz "
+        },
+        {
+           ingredient: "Blue Curacao",
+           measure: "1 oz "
+        },
+        {
+           ingredient: "Lime juice",
+           measure: "1 oz "
+        },
+        {
+           ingredient: "Salt",
+           measure: "Coarse "
+        }
+    ],
+   imageSource: null,
+   imageAttribution: null,
+   creativeCommonsConfirmed: "Yes",
+   dateModified: "2015-08-18 14:51:53"
 };
