96a8c86cad2c54fa57b3a62448a2a8453c2fc5ec
Johan Angelstam <johan@angelstam.se>
2024-09-11 11:30:59 +0200

    Update the TheCocktailDB endpoint random with a real implementation.

--- src/data/TheCocktailDB.ts
+++ src/data/TheCocktailDB.ts
@@ -22,10 +22,31 @@ export interface Ingredient {
     measure: String | null;
 }
 
+const THE_COCKTAIL_DB_API_BASE_URL: string = "https://www.thecocktaildb.com/api/json/v1/1/";
+
 
 export async function random() {
-    return new Promise<Drink>((resolve, _) => {
-        resolve(DRINK_1);
+    const ENDPOINT_URI: string = "random.php";
+
+    return new Promise<Drink>(async (resolve, reject) => {
+        try {
+            const response = await fetch(THE_COCKTAIL_DB_API_BASE_URL + ENDPOINT_URI);
+            if (!response.ok) {
+                console.log("Network response was not ok");
+                reject("Network response was not ok.");
+            }
+            const data = await response.json();
+
+            if (data.drinks[0]) {
+                resolve(transformToDrink(data.drinks[0]));
+            } else {
+                console.log("No drinks in response", data);
+                reject("Sorry, no drinks for you.");
+            }
+        } catch (error) {
+            console.error("There has been a problem with your fetch operation:", error);
+            reject("There has been a problem with your fetch operation.");
+        }
     });
 }
 
@@ -33,6 +54,46 @@ export async function search(query: String) {
     return new Promise<Array<Drink>>((resolve, _) => {
         resolve([DRINK_2, DRINK_3]);
     });
+}
+
+function transformToDrink(data: any) : Drink {
+    return {
+        id: data.idDrink,
+        drink: data.strDrink,
+        drinkAlternate: data.strDrinkAlternate,
+        tags: data.strTags,
+        video: data.strVideo,
+        category: data.strCategory,
+        IBA: data.strIBA,
+        alcoholic: data.strAlcoholic,
+        glass: data.strGlass,
+        instructions: data.strInstructions,
+        drinkThumb: data.strDrinkThumb,
+        ingredients: transformToIngredients(data),
+        imageSource: data.strImageSource,
+        imageAttribution: data.strImageAttribution,
+        creativeCommonsConfirmed: data.strCreativeCommonsConfirmed,
+        dateModified: data.dateModified
+    };
+}
+
+function transformToIngredients(data: any) : Array<Ingredient> {
+    let ingredients: Array<Ingredient> = [];
+
+    for (let i = 1; i <= 15; i++) {
+        let strIngredientX = "strIngredient" + i;
+
+        if (data[strIngredientX] !== null) {
+        let strMeasureX = "strMeasure" + i;
+
+        ingredients.push({
+                ingredient: data[strIngredientX],
+                measure: data[strMeasureX]
+            });
+        }
+    }
+
+    return ingredients;
 }
 
 // Temporary data for test
