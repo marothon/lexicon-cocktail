b0d02335f1e22f9cf77a487e78dea6332b663009
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-09-12 09:14:30 +0200

    Draft mobile menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,43 +6,47 @@ export function App() {
     
     return (
         <>
-        <header>
-            <h1>CocktailDB</h1>
-            <section className="headerLinks">
+            <header>
+                <h1>CocktailDB</h1>
+                <section className="headerLinks">
 
-                <NavLink 
-                    to="/" 
-                    className={({ isActive }) =>
-                        [
-                            isActive ? "active" : ""
-                        ].join(" ")
-                    }
-                >
-                Home
-                </NavLink>
+                    <NavLink 
+                        to="/" 
+                        className={({ isActive }) =>
+                            [
+                                isActive ? "active" : ""
+                            ].join(" ")
+                        }
+                    >
+                    Home
+                    </NavLink>
 
-                <NavLink 
-                    to="search" 
-                    className={({ isActive }) =>
-                        [
-                            isActive ? "active" : ""
-                        ].join(" ")
-                    }
-                >
-                Search
-                </NavLink>
+                    <NavLink 
+                        to="search" 
+                        className={({ isActive }) =>
+                            [
+                                isActive ? "active" : ""
+                            ].join(" ")
+                        }
+                    >
+                    Search
+                    </NavLink>
 
-            </section>
-            
-        </header>
+                </section>
 
-        <main>
-            <Outlet />
-        </main>
+                <section id="burgerMenu" className="material-symbols-outlined">
+                    menu
+                </section>
+                
+            </header>
 
-        <footer>
-            <h1>Robo Surfers</h1>
-        </footer>
+            <main>
+                <Outlet />
+            </main>
+
+            <footer>
+                <h1>Robo Surfers</h1>
+            </footer>
         </>
     )
     }
