4390b5d7991a0deeaf10a50b658c7a3fdda911b1
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-09-16 11:56:11 +0200

    Further implementation of GlobalStateContext

--- src/context/GlobalStateContext.tsx
+++ src/context/GlobalStateContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import * as CocktailDB from "../data/TheCocktailDB";
 
 interface DrinkFavorite {
   id: string;
@@ -8,23 +9,35 @@ interface DrinkFavorite {
 
 interface GlobalState {
   favorites: DrinkFavorite[];
-  toggleFavorite: (id: string) => void; 
+  toggleFavorite: (id: CocktailDB.Drink) => void; 
 }
 
 const defaultGlobalContext: GlobalState = {
   favorites: [],
   toggleFavorite: () => {}
 }
-const GlobalStateContext = createContext(defaultGlobalContext);
+const GlobalStateContext = createContext<GlobalState>(defaultGlobalContext);
 
 export function GlobalStateProvider ( {children}: {children: ReactNode}) {
   const [favorites, setFavorites] = useState<Map<string, DrinkFavorite>>(new Map<string, DrinkFavorite>());
   
-  function toggleFavorite(id: string){
-    return
+  function toggleFavorite(drink: CocktailDB.Drink){
+    if(favorites.has(drink.id)){
+      setFavorites((oldFavorites) => {
+        oldFavorites.delete(drink.id);
+        return structuredClone(oldFavorites);
+      })
+      
+    }
+    else{
+      setFavorites((oldFavorites) => {
+        oldFavorites.set(drink.id, {id: drink.id, drink: drink.drink, drinkThumb: drink.drinkThumb});
+        return structuredClone(oldFavorites);
+      })
+    }
   }
 
-  let data = {
+  let data: GlobalState = {
     favorites: Array.from(favorites.values()),
     toggleFavorite: toggleFavorite
   };
