14ff5112ae1673a5a7f364d2c42c4b88825ce04f
arbzeq <arbnor.zeqiri@yahoo.com>
2024-09-20 14:41:07 +0200

    Add resposive mobile-menu (#42)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,32 @@
+import { ReactNode, useState } from 'react';
 import { NavLink } from 'react-router-dom'
 
-export default function Header() {
+function HeaderLinks ( { show } : { show?: boolean}) : ReactNode {
   return (
-    <header>
-        <nav>
-          <h1>CocktailDB</h1>
-          <section className="headerLinks">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                [
-                  isActive ? "active" : ""
-                ].join(" ")
-              }
-            >
-              Home
-            </NavLink>
+    <section className={`headerLinks ${show ? "": 'hide'}`}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            [
+              isActive ? "active" : ""
+            ].join(" ")
+          }
+        >
+          Home
+        </NavLink>
 
-            <NavLink
-              to="search"
-              className={({ isActive }) =>
-                [
-                  isActive ? "active" : ""
-                ].join(" ")
-              }
-            >
-              Search
-            </NavLink>
+        <NavLink
+          to="search"
+          className={({ isActive }) =>
+            [
+              isActive ? "active" : ""
+            ].join(" ")
+          }
+        >
+          Search
+        </NavLink>
 
-            <NavLink
+        <NavLink
               to="favorites?p=1"
               className={({ isActive }) =>
                 [
@@ -37,13 +35,37 @@ export default function Header() {
               }
             >
               Favorites
-            </NavLink>
+        </NavLink>
+    </section>
+  )
+}
 
-          </section>
-          <section id="burgerMenu" className="material-symbols-outlined">
+export default function Header() {
+  const [ showMobileNavigation, setShowMobileNavigation ] = useState<boolean>(false);
+
+  function showMobileNavigationMenu() {
+    setShowMobileNavigation(!showMobileNavigation);
+  }
+
+  return (
+    <header> 
+      <div className="headerContainer desktop">
+        <nav className="headerNavigation">
+          <h1>CocktailDB</h1>
+          <HeaderLinks />
+        </nav>
+      </div>
+      
+      <div className="headerContainer mobile">
+        <nav className="headerNavigation">
+          <h1>CocktailDB</h1>
+          <section id="burgerMenu" className="material-symbols-outlined" onClick={ showMobileNavigationMenu }>
             menu
           </section>
         </nav>
-      </header>
-  )
+        <HeaderLinks show={showMobileNavigation}/>
+      </div>
+
+    </header>
+  );
 }
