c376956b61dd9e1e2c91c83e992a64da41012d53
Johan Angelstam <johan@angelstam.se>
2024-09-30 12:01:20 +0200

    Refactor parameters to requestEndpoint into interface EndpointConfig<T> for improved readability in TheCocktailDB.

--- src/data/TheCocktailDB.ts
+++ src/data/TheCocktailDB.ts
@@ -6,133 +6,140 @@ const THE_COCKTAIL_DB_API_BASE_URL: string = "https://www.thecocktaildb.com/api/
 
 
 export async function random() : Promise<Drink> {
-    return requestEndpoint<Drink>(
-        "random.php",
-        data => data.drinks[0],
-        data => transformToDrink(data.drinks[0])
-    );
+    return requestEndpoint<Drink>({
+        endpointUri: "random.php",
+        test: data => data.drinks[0],
+        transform: data => transformToDrink(data.drinks[0])
+    });
 }
 
 export async function search(query: string) : Promise<Array<Drink>> {
-    return requestEndpoint<Array<Drink>>(
-        "search.php",
-        data => Array.isArray(data.drinks),
-        data => data.drinks.map(transformToDrink),
-        [{key: "s", value: query}]
-    );
+    return requestEndpoint<Array<Drink>>({
+        endpointUri: "search.php",
+        test: data => Array.isArray(data.drinks),
+        transform: data => data.drinks.map(transformToDrink),
+        parameters: [{key: "s", value: query}]
+    });
 }
 
 export async function searchIngredient(query: string) : Promise<Ingredient> {
-    return requestEndpoint<Ingredient>(
-        "search.php",
-        data => data.ingredients[0],
-        data => transformToIngredient(data.ingredients[0]),
-        [{key: "i", value: query}],
-        true
-    );
+    return requestEndpoint<Ingredient>({
+        endpointUri: "search.php",
+        test: data => data.ingredients[0],
+        transform: data => transformToIngredient(data.ingredients[0]),
+        parameters: [{key: "i", value: query}],
+        useCache: true
+    });
 }
 
 export async function lookupDrink(id: string) : Promise<Drink> {
-    return requestEndpoint<Drink>(
-        "lookup.php",
-        data => data.drinks[0],
-        data => transformToDrink(data.drinks[0]),
-        [{key: "i", value: id}],
-        true
-    );
+    return requestEndpoint<Drink>({
+        endpointUri: "lookup.php",
+        test: data => data.drinks[0],
+        transform: data => transformToDrink(data.drinks[0]),
+        parameters: [{key: "i", value: id}],
+        useCache: true
+    });
 }
 
 export async function lookupIngredient(id: string) : Promise<Ingredient> {
-    return requestEndpoint<Ingredient>(
-        "lookup.php",
-        data => data.ingredients[0],
-        data => transformToIngredient(data.ingredients[0]),
-        [{key: "iid", value: id}],
-        true
-    );
+    return requestEndpoint<Ingredient>({
+        endpointUri: "lookup.php",
+        test: data => data.ingredients[0],
+        transform: data => transformToIngredient(data.ingredients[0]),
+        parameters: [{key: "iid", value: id}],
+        useCache: true
+    });
 }
 
 export async function filterByIngredient(ingredientName: string) : Promise<Drink[]> {
-    return requestEndpoint<Drink[]>(
-        'filter.php',
-        data => data.drinks,
-        data => data.drinks.map((d: any) => transformToDrink(d)),
-        [{key: 'i', value: ingredientName}],
-        true
-    );
+    return requestEndpoint<Drink[]>({
+        endpointUri: 'filter.php',
+        test: data => data.drinks,
+        transform: data => data.drinks.map((d: any) => transformToDrink(d)),
+        parameters: [{key: 'i', value: ingredientName}],
+        useCache: true
+    });
 }
 
 export async function allIngredients() : Promise<string[]> {
-    return requestEndpoint<string[]>(
-        'list.php',
-        data => data.drinks,
-        data => data.drinks.map((d: any) => d.strIngredient1),
-        [{key: 'i', value: 'list'}],
-        true
-    );
+    return requestEndpoint<string[]>({
+        endpointUri: 'list.php',
+        test: data => data.drinks,
+        transform: data => data.drinks.map((d: any) => d.strIngredient1),
+        parameters: [{key: 'i', value: 'list'}],
+        useCache: true
+    });
 }
 
 export async function allCategories() : Promise<string[]> {
-    return requestEndpoint<string[]>(
-        'list.php',
-        data => data.drinks,
-        data => data.drinks.map((d: any) => d.strCategory),
-        [{key: 'c', value: 'list'}],
-        true
-    );
+    return requestEndpoint<string[]>({
+        endpointUri: 'list.php',
+        test: data => data.drinks,
+        transform: data => data.drinks.map((d: any) => d.strCategory),
+        parameters: [{key: 'c', value: 'list'}],
+        useCache: true
+    });
 }
 
 export async function allGlasses() : Promise<string[]> {
-    return requestEndpoint<string[]>(
-        'list.php',
-        data => data.drinks,
-        data => data.drinks.map((d: any) => d.strGlass),
-        [{key: 'g', value: 'list'}],
-        true
-    );
+    return requestEndpoint<string[]>({
+        endpointUri: 'list.php',
+        test: data => data.drinks,
+        transform: data => data.drinks.map((d: any) => d.strGlass),
+        parameters: [{key: 'g', value: 'list'}],
+        useCache: true
+    });
 }
 
 export async function allAlcoholTypes() : Promise<string[]> {
-    return requestEndpoint<string[]>(
-        'list.php',
-        data => data.drinks,
-        data => data.drinks.map((d: any) => d.strAlcoholic),
-        [{key: 'a', value: 'list'}],
-        true
-    );
+    return requestEndpoint<string[]>({
+        endpointUri: 'list.php',
+        test: data => data.drinks,
+        transform: data => data.drinks.map((d: any) => d.strAlcoholic),
+        parameters: [{key: 'a', value: 'list'}],
+        useCache: true
+    });
 }
 
-async function requestEndpoint<T>(endpointUri: string, test: (data: any) => boolean, transform: (data: any) => T,
-    parameters: Array<{key: string, value: string}> = [], useCache: boolean = false): Promise<T> {
+interface EndpointConfig<T> {
+    endpointUri: string;
+    test: (data: any) => boolean;
+    transform: (data: any) => T;
+    parameters?: Array<{key: string, value: string}>;
+    useCache?: boolean;
+}
+
+async function requestEndpoint<T>(config: EndpointConfig<T>): Promise<T> {
     return new Promise<T>(async (resolve, reject) => {
         try {
             let paramString: string = "";
-            if (parameters.length > 0) {
-                paramString = "?" + parameters.map(
+            if (config.parameters && config.parameters.length > 0) {
+                paramString = "?" + config.parameters.map(
                         (parameter) => encodeURI(parameter.key) + "=" + encodeURI(parameter.value)
                     ).join();
             }
 
-            let cacheId = endpointUri + paramString;
+            let cacheId = config.endpointUri + paramString;
             let data;
 
-            if (useCache && Cache.exists(cacheId)) {
+            if (config.useCache && Cache.exists(cacheId)) {
                 data = Cache.retrieve(cacheId);
             } else {
-                const response = await fetch(THE_COCKTAIL_DB_API_BASE_URL + endpointUri + paramString);
+                const response = await fetch(THE_COCKTAIL_DB_API_BASE_URL + config.endpointUri + paramString);
                 if (!response.ok) {
                     console.log("Network response was not ok");
                     reject("Network response was not ok.");
                 }
                 data = await response.json();
 
-                if (useCache) {
+                if (config.useCache) {
                     Cache.store(cacheId, data);
                 }
             }
 
-            if (test(data)) {
-                resolve(transform(data));
+            if (config.test(data)) {
+                resolve(config.transform(data));
             } else {
                 console.log("No drinks in response", data);
                 reject("Sorry, no drinks for you.");
